468235b8a54f5e45848e09ad171edbf5a8de42ca
Suz Hinton <suz.hinton@gmail.com>
2015-08-12 21:48:38 -0400

    umm no sudo required

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ echo "installing and rebuilding usb for nw use..."
 cd node_modules/avrgirl-ispmkii/node_modules
 npm install usb
 cd usb
-sudo node-pre-gyp configure --runtime=node-webkit --target=0.12.3
-sudo node-pre-gyp build --runtime=node-webkit --target=0.12.3
+node-pre-gyp configure --runtime=node-webkit --target=0.12.3
+node-pre-gyp build --runtime=node-webkit --target=0.12.3
 echo "all done."
