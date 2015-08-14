81d656a6ab11843e138efb3982a54e86b4d909da
Suz Hinton <suz.hinton@gmail.com>
2015-08-13 23:21:59 -0400

    simpify setup

--- setup.sh
+++ setup.sh
@@ -8,9 +8,7 @@ do
 done
 echo "npm installed the things."
 echo "installing and rebuilding usb for nw use..."
-cd node_modules/avrgirl-ispmkii/node_modules
-npm install usb
-cd usb
+cd node_modules/avrgirl-ispmkii/node_modules/usb
 node-pre-gyp configure --runtime=node-webkit --target=0.12.3
 node-pre-gyp build --runtime=node-webkit --target=0.12.3
 echo "all done."
